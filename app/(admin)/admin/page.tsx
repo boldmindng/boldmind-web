b884f0b981ab328d0f618f32614ae5516acd5d9c
charlesuchijuka <uchecharles223@gmail.com>
2026-07-14 22:36:42 +0100

    bug refiner

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,7 +1,4 @@
-import type { Metadata } from 'next';
-import AdminOverviewClient from './AdminOverviewClient';
-
-export const metadata: Metadata = { title: 'Admin Overview' };
+import AdminOverviewClient from "./AdminOverviewClient";
 
 export default function AdminPage() {
   return <AdminOverviewClient />;
